41123966dc98d428c09c9e328225ca6255c2d3f1
Melgurt <melker.vallen@gmail.com>
2024-03-27 16:07:47 +0100

    Texten vill inte funka

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import Phaser from "phaser";
 import titleScreen from "./scenes/title_screen";
 
 const config = {
-  width: 1900,
-  height: 930,
+  width: window.innerWidth,
+  height: window.innerHeight,
   type: Phaser.AUTO,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
 
 const game = new Phaser.Game(config);
+
+window.addEventListener("resize", function () {
+  game.scale.resize(window.innerWidth, window.innerHeight);
+});
 
 game.scene.add("titlescreen", titleScreen);
 
--- src/scenes/title_screen.js
+++ src/scenes/title_screen.js
@@ -4,7 +4,10 @@ export default class titleScreen extends Phaser.Scene {
   prelead() {}
 
   create() {
-    const text = this.add.text(400, 250, "Hello World!");
-    text.setOrigin(-4, 0.5);
+    const screenCenterX = this.cameras.main.width / 2;
+    const screenCenterY = this.cameras.main.height / 2;
+    const menuText = this.add
+      .text(screenCenterX, screenCenterY, "Hello World!")
+      .setOrigin(0.5, 0.5);
   }
 }
