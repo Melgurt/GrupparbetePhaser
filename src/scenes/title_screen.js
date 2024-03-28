bbe06c3cac871e26f6d6afaf0a8a46b788c37ab8
Melgurt <melker.vallen@gmail.com>
2024-03-28 09:07:30 +0100

    blub

--- src/scenes/title_screen.js
+++ src/scenes/title_screen.js
@@ -2,14 +2,21 @@ import Phaser from "phaser";
 
 export default class titleScreen extends Phaser.Scene {
   preload() {
-    this.load.image("background", "Backgrounds/game_background_3. 2.png");
+    this.load.image(
+      "titleScreenBackground",
+      "Backgrounds/game_background_3.png"
+    );
   }
 
   create() {
+    const titleScreenBackground = this.add.image(0, 0, "titleScreenBackground");
+
     const screenCenterX = this.cameras.main.width / 2;
     const screenCenterY = this.cameras.main.height / 2;
     const menuText = this.add
       .text(screenCenterX, screenCenterY, "Hello World!")
       .setOrigin(0.5, 0.5);
   }
+
+  update() {}
 }
